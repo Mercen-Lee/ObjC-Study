ace5f79f0edaa677bc7e8e109032f5de3c98d141
이석호 <mercen@mercen.net>
2022-10-05 10:35:52 +0900

    ADD: Increase Decrease Action

--- ObjCStudy/ViewController.m
+++ ObjCStudy/ViewController.m
@@ -9,6 +9,9 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *NumericLabel;
+@property int count;
+
 @end
 
 @implementation ViewController
@@ -18,5 +21,14 @@
     // Do any additional setup after loading the view.
 }
 
+- (IBAction)Increase:(UIButton *)sender {
+    self.count++;
+    self.NumericLabel.text = [NSString stringWithFormat:@"%d", self.count];;
+}
+
+- (IBAction)Decrease:(UIButton *)sender {
+    self.count--;
+    self.NumericLabel.text = [NSString stringWithFormat:@"%d", self.count];;
+}
 
 @end
